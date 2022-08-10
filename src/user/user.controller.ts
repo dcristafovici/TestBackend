c878b49e3f9abc53a839425452954ddb000782b6
denibudeyko <cristafovici.den@gmail.com>
2022-08-10 13:36:29 +0300

    Login controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,6 +11,11 @@ export class UserController {
     return this.userService.getAll();
   }
 
+  @Post('/login')
+  async login(@Req() req): Promise<User> {
+    return this.userService.login(req.body);
+  }
+
   @Post('/register')
   async registerUser(@Req() req): Promise<User> {
     return this.userService.register(req.body);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,7 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import { Repository } from 'typeorm';
-import { UserRegisterModel } from './user.dto';
+import { UserLoginModel, UserRegisterModel } from './user.dto';
 import { User } from './user.entity';
 
 @Injectable()
@@ -34,5 +34,36 @@ export class UserService {
 
   getAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async login(data: UserLoginModel): Promise<User> {
+    const { password, email } = data;
+
+    const existedUser: User = await this.userRepository.findOne({
+      where: { email },
+    });
+
+    if (!existedUser) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          message: 'User not registered.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const matchPassword = await bcrypt.compare(password, existedUser.password);
+
+    if (!matchPassword) {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNAUTHORIZED,
+          message: 'Invalid credentials.',
+        },
+        HttpStatus.UNAUTHORIZED,
+      );
+    }
+
+    return this.userRepository.findOne({ where: { email } });
   }
 }
